307892067ec4bd49606e4fd76db4c11e83293ddf
khaledakel <kakel.ieu2021@student.ie.edu>
2025-02-26 16:54:39 +0100

    Add theme colors and apply background color to HomePage

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import '../../theme/index.dart' show AppColors;
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
         title: Text('Home Page'),
       ),
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff40E0D0);
+  static const background = Color(0xffffffff);
+}

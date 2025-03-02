3b2df5d18b0b6947f4500228bd084a3f9185e6f7
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-02 12:14:13 +0100

    Add Location and User model classes with required fields

---
+++ lib/models/location.dart
@@ -0,0 +1,9 @@
+class Location {
+  final int id;
+  final String name;
+  final String address;
+  final String city;
+  final String country;
+
+  Location({required this.id, required this.name, required this.address, required this.city, required this.country});
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+class User {
+  final int id;
+  final String name;
+  final String avatar;
+  final int locationId;
+  final String biography;
+
+  User({required this.id, required this.name, required this.avatar, required this.locationId, required this.biography});
+
+
+
+}

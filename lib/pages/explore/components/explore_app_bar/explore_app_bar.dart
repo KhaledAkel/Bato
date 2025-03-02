8b5979931a05d387c6926820b03be01b2fd0d734
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-02 17:01:28 +0100

    Add ExploreAppBar and ExplorePage components for navigation and layout

---
+++ lib/pages/explore/components/explore_app_bar/explore_app_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../../../../components/index.dart' show Logo;
+import '../../../../theme/index.dart' show AppColors;
+
+class ExploreAppBar extends StatelessWidget implements PreferredSizeWidget {
+
+  const ExploreAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Logo(size: 40.0),
+      backgroundColor: AppColors.background,
+      elevation: 0.0,
+      scrolledUnderElevation: 0,
+      
+      actions: [
+        IconButton(
+          icon: Icon(Icons.notifications_none),
+          iconSize: 25,
+          color: AppColors.text,
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.message_outlined),
+          color: AppColors.text,
+          iconSize: 25,
+          onPressed: () {},
+        ),
+      ],
+      
+    );
+  }
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
---
+++ lib/pages/explore/components/index.dart
@@ -0,0 +1,1 @@
+export './explore_app_bar/explore_app_bar.dart';

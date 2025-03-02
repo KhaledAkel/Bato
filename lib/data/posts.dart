4de6b7c4a8caa4d63ec3794c9dff41a6845a825e
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-02 14:32:09 +0100

    Update post image URLs in posts data; remove outdated image link

--- lib/data/posts.dart
+++ lib/data/posts.dart
@@ -9,8 +9,8 @@ final List<Post> posts = [
     postLocation: 'New York, USA',
     postOwnerProfileUrl: usersList[0].avatar,
     imageUrls: [
+      'https://res.cloudinary.com/doiav30hi/image/upload/v1740920756/samples/people/jazz.jpg',
       'https://res.cloudinary.com/doiav30hi/image/upload/v1740920835/m2ztm2v27q9fs7ubbq22.jpg',
-      'https://res.cloudinary.com/doiav30hi/image/upload/v1740920835/kpm2ppf4jufpunmzdcqm.jpg',
       'https://res.cloudinary.com/doiav30hi/image/upload/v1740920835/xusb4pp56dsh2vnflfjb.jpg',
     ],
     postCaption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.',

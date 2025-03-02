a12341675a741e39e324d9b120b2efb02d1f45a8
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-02 12:23:57 +0100

    Add user and location data files with sample entries and export models

---
+++ lib/data/locations.dart
@@ -0,0 +1,18 @@
+import '../models/index.dart' show Location;
+
+List <Location> locationsList = [
+  Location(
+    id: 0,
+    name: 'Notrocal',
+    address: '1234 Elm St',
+    city: 'Springfield',
+    country: 'USA'
+  ),
+  Location(
+    id: 1,
+    name: 'Soutrocal',
+    address: '5678 Oak St',
+    city: 'Shelbyville',
+    country: 'USA'
+  ),
+];
---
+++ lib/data/users.dart
@@ -0,0 +1,19 @@
+import '../models/index.dart' show User;
+
+List<User> usersList = [
+  User(
+    id: 0,
+    name: 'John Doe',
+    avatar: 'https://images.pexels.com/photos/845457/pexels-photo-845457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    locationId: 0,
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  ),
+  User(
+    id: 1,
+    name: 'Jane Smith',
+    avatar: 'https://images.pexels.com/photos/2896428/pexels-photo-2896428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    locationId: 1,
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  ),
+
+];

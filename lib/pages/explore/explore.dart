b005d7cc1e6d585580bfd8747faaea584c6861a7
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-03 13:06:11 +0100

    Add tabbed navigation to ExplorePage for improved content organization

--- lib/pages/explore/explore.dart
+++ lib/pages/explore/explore.dart
@@ -1,14 +1,90 @@
 import 'package:flutter/material.dart';
-
-
+import '../../theme/index.dart' show AppColors, AppTextStyles;
 
 class ExplorePage extends StatelessWidget {
   const ExplorePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text('Explore Page')
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Column(
+        children: [
+          // TabBar can be placed here in the body
+          TabBar(
+            tabs:  [
+              Text('Trending', style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                fontFamily: AppTextStyles.fontFamilyPrimary,
+              ),),
+              Text('Companions', style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                fontFamily: AppTextStyles.fontFamilyPrimary,
+              ),),
+              Text('Hosts', style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                fontFamily: AppTextStyles.fontFamilyPrimary,
+              ),),
+
+            ],
+            splashFactory: NoSplash.splashFactory,
+            overlayColor: WidgetStateProperty.resolveWith<Color?>(
+              (Set<WidgetState> states) {
+                return states.contains(WidgetState.focused)
+                    ? null
+                    : Colors.transparent;
+              },
+            ),
+            
+            labelPadding: EdgeInsets.all(12),
+            unselectedLabelColor: Colors.grey,
+            indicatorColor: AppColors.primary,
+            labelColor: AppColors.primary,
+          ),
+          // TabBarView to show content for each tab
+          Expanded(
+            child: TabBarView(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: ListView(
+                    children: [
+                      
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 16,
+                  ),
+                  child: ListView(
+                    children: [
+                     
+                    ],
+                  ),
+                  
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 16,
+                  ),
+                  child: ListView(
+                    children: [
+                     
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

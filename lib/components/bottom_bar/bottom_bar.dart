c9e9e5752504e91ba3abeed37eba1b9eff1281f6
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-03 12:14:26 +0100

    Refactor BottomBar component to use stateless widget and simplify item selection logic

--- lib/components/bottom_bar/bottom_bar.dart
+++ lib/components/bottom_bar/bottom_bar.dart
@@ -1,50 +1,35 @@
 import 'package:flutter/material.dart';
 import '../../theme/index.dart' show AppColors;
 
-class BottomBar extends StatefulWidget {
-  final ValueChanged<int>? onItemSelected;
+class BottomBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
 
-  BottomBar({super.key, this.onItemSelected});
-
-  @override
-  State<BottomBar> createState() => _BottomBarState();
-}
-
-class _BottomBarState extends State<BottomBar> {
-  int selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      selectedIndex = index;
-    });
-    widget.onItemSelected?.call(index); // Notify parent if callback is set
-  }
+  const BottomBar({super.key, required this.currentIndex, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
+    final icons = [
+      Icons.home,
+      Icons.explore,
+      Icons.add,
+      Icons.airplane_ticket,
+      Icons.account_circle,
+    ];
+
     return BottomAppBar(
-      height: 59.0,
       shape: const CircularNotchedRectangle(),
       color: AppColors.background,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: List.generate(5, (index) {
-          final icons = [
-            Icons.home_outlined,
-            Icons.explore_outlined,
-            Icons.add,
-            Icons.airplane_ticket_outlined,
-            Icons.account_circle_outlined,
-          ];
-
+        children: List.generate(icons.length, (index) {
           return IconButton(
             icon: Icon(icons[index]),
             iconSize: 33,
             splashColor: Colors.transparent,
             highlightColor: Colors.transparent,
-            color: selectedIndex == index ? AppColors.primary : AppColors.text,
-            onPressed: () => _onItemTapped(index),
+            color: currentIndex == index ? AppColors.primary : AppColors.text,
+            onPressed: () => onTap(index), // Switch tab when pressed
           );
         }),
       ),

0f8f87f421ef779839be9213ddbdbb5303ca2e6c
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-03 12:14:42 +0100

    Refactor ExplorePage and HomePage components for improved code clarity and structure

--- lib/pages/explore/explore.dart
+++ lib/pages/explore/explore.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+
+
 class ExplorePage extends StatelessWidget {
-  const ExplorePage({Key? key}) : super(key: key);
+  const ExplorePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Explore Page'),
+        child: Text('Explore Page')
     );
   }
 }
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,31 +2,26 @@ import 'package:flutter/material.dart';
 import '../../theme/index.dart' show AppColors;
 import 'components/index.dart' show HomeAppBar, HomePost;
 import '../../data/index.dart' show posts;
-import '../../components/index.dart' show BottomBar;
+
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.background,
-
-      appBar: HomeAppBar(),
-      body: ListView.builder( itemCount: posts.length,
-            itemBuilder: (context, index) {
-              final post = posts[index];
-              return HomePost(
-                id: post.id,
-                postOwnerName: post.postOwnerName,
-                postLocation: post.postLocation,
-                postProfileImageUrl: post.postOwnerProfileUrl,
-                imageUrls: post.imageUrls,
-                postCaption: post.postCaption,
-              );
-            },
-      ),
-
-    bottomNavigationBar: BottomBar(),
-    );
+    return ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) {
+          final post = posts[index];
+          return HomePost(
+            id: post.id,
+            postOwnerName: post.postOwnerName,
+            postLocation: post.postLocation,
+            postProfileImageUrl: post.postOwnerProfileUrl,
+            imageUrls: post.imageUrls,
+            postCaption: post.postCaption,
+          );
+        },
+      );
   }
 }

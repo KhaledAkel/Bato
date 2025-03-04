9c7fee154f4ac9601ddf69171c9987378a61c130
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-04 14:09:55 +0100

    Refactor PostBottomBar to enhance layout and user interaction; add PostMessageButton for improved messaging functionality

--- lib/pages/explore/screens/companions/components/companion_post/blocks/post_bottom_bar.dart
+++ lib/pages/explore/screens/companions/components/companion_post/blocks/post_bottom_bar.dart
@@ -7,50 +7,27 @@ class PostBottomBar extends StatefulWidget {
 }
 
 class _PostBottomBarState extends State<PostBottomBar> {
-  bool isLiked = false;
-  bool isSaved = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(
-                  isLiked ? Icons.favorite : Icons.favorite_border,
-                  color: isLiked ? Colors.red : null,
-                ),
-                onPressed: () {
-                  setState(() {
-                    isLiked = !isLiked;
-                  });
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.comment),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.send),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          IconButton(
-            icon: Icon(
-              isSaved ? Icons.bookmark : Icons.bookmark_border,
-              color: isSaved ? AppColors.primary : null,
-            ),
-            onPressed: () {
-              setState(() {
-                isSaved = !isSaved;
-              });
-            },
-          ),
+          TextButton.icon(
+      onPressed: () {},
+      icon: const Icon(Icons.send, color: AppColors.primary),
+      label: const Text("Send a message", style: TextStyle(color: AppColors.primary)),
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+
+
         ],
       ),
     );

d180bec2d12ab60a80edb6af3350891392057802
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-05 14:43:22 +0100

    Add PlanTripAppBar component for trip planning interface with custom styling and actions

---
+++ lib/pages/plan_trip/components/plan_trip_app_bar/plan_trip_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../../../../components/index.dart' show Logo;
+import '../../../../theme/index.dart' show AppColors, AppTextStyles;
+
+class PlanTripAppBar extends StatelessWidget implements PreferredSizeWidget {
+
+  const PlanTripAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+      'Bato',
+      style: TextStyle(
+        fontFamily:
+            AppTextStyles.fontFamilyPrimary, // Use the primary font family
+        fontSize: 40.0, // Use size to set the font size
+        fontWeight: FontWeight.w900,
+        color: AppColors.background, // You can customize the color as needed
+      ),
+    ),
+      backgroundColor: AppColors.primary,
+      elevation: 0.0,
+      scrolledUnderElevation: 0,
+      
+      actions: [
+        IconButton(
+          icon: Icon(Icons.book_outlined),
+          iconSize: 25,
+          color: AppColors.background,
+          onPressed: () {},
+        ),
+      ],
+      
+    );
+  }
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}

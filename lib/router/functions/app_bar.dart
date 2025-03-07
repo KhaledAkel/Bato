e9f92d60e4a2b6c8175243e94c73474e33c06376
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-07 17:39:57 +0100

    Add MyProfileAppBar to app bar routing for user profile navigation

--- lib/router/functions/app_bar.dart
+++ lib/router/functions/app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../pages/home/components/index.dart' show HomeAppBar;
 import '../../pages/explore/components/index.dart' show ExploreAppBar;
 import '../../pages/plan_trip/components/index.dart' show PlanTripAppBar;
+import '../../pages/profile/index.dart' show MyProfileAppBar;
 
 PreferredSizeWidget? getAppBar(BuildContext context, String? fullPath) {
   switch (fullPath) {
@@ -13,6 +14,9 @@ PreferredSizeWidget? getAppBar(BuildContext context, String? fullPath) {
 
     case '/plan_trip':
       return PlanTripAppBar();
+
+    case '/my_profile':
+      return MyProfileAppBar();
       
       
     default:

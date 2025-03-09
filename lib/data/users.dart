e8b69fb38a55146959d33d3a75efd74e33f7647c
khaledakel <kakel.ieu2021@student.ie.edu>
2025-03-09 18:27:49 +0100

    Add imageUrls, trips, and host fields to User model and update usersList

--- lib/data/users.dart
+++ lib/data/users.dart
@@ -5,13 +5,31 @@ List<User> usersList = [
     id: 0,
     name: 'John Doe',
     avatar: 'https://res.cloudinary.com/doiav30hi/image/upload/v1740920764/cld-sample.jpg',
-    biography: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    imageUrls: [
+      'https://res.cloudinary.com/doiav30hi/image/upload/v1740920764/cld-sample.jpg',
+      'https://res.cloudinary.com/doiav30hi/image/upload/v1740920754/samples/food/dessert.jpg',
+      'https://res.cloudinary.com/doiav30hi/image/upload/v1740920762/samples/balloons.jpg',
+    ],
+    trips : [
+    ],
+
+    host: [],
   ),
   User(
     id: 1,
     name: 'Jane Smith',
     avatar: 'https://images.pexels.com/photos/2896428/pexels-photo-2896428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    biography: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    imageUrls: [
+      'https://res.cloudinary.com/doiav30hi/image/upload/v1740920764/cld-sample.jpg',
+      'https://res.cloudinary.com/doiav30hi/image/upload/v1740920754/samples/food/dessert.jpg',
+      'https://res.cloudinary.com/doiav30hi/image/upload/v1740920762/samples/balloons.jpg',
+    ],
+    trips : [
+    ],
+
+    host: [],
   ),
 
 ];
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,8 +3,11 @@ class User {
   final String name;
   final String avatar;
   final String biography;
+  final List<String> imageUrls;
+  final List<int> trips;
+  final List<int> host;
 
-  User({required this.id, required this.name, required this.avatar, required this.biography});
+  User({required this.id, required this.name, required this.avatar, required this.biography, required this.imageUrls, required this.trips, required this.host});
 
 
 
